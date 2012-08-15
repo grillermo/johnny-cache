d7bb9b6e63fed066db51521063decfe458ef9475
Guillermo Siliceo Trueba <guillermo.siliceo@gmail.com>
2012-08-14 21:12:38 -0500

    Added a new johnny cache backend with django_pylibmc.memcached support

--- johnny/backends/memcached.py
+++ johnny/backends/memcached.py
@@ -7,6 +7,7 @@ django's default backend modules.
 
 import django
 from django.core.cache.backends import memcached
+from django_pylibmc.memcached import PyLibMCCache as PyLibMCCacheSasl
 
 
 class CacheClass(memcached.CacheClass):
@@ -32,6 +33,36 @@ if django.VERSION[:2] > (1, 2):
             return super(MemcachedCache, self)._get_memcache_timeout(timeout)
 
     class PyLibMCCache(memcached.PyLibMCCache):
+        """
+        PyLibMCCache version that interprets 0 to mean, roughly, 30 days.
+        This is because `pylibmc interprets 0 to mean literally zero seconds
+        <http://sendapatch.se/projects/pylibmc/misc.html#differences-from-python-memcached>`_
+        rather than "infinity" as memcached itself does.  The maximum timeout
+        memcached allows before treating the timeout as a timestamp is just
+        under 30 days.
+        """
+        def _get_memcache_timeout(self, timeout=None):
+            # pylibmc doesn't like our definition of 0
+            if timeout == 0:
+                return 2591999
+            return super(PyLibMCCache, self)._get_memcache_timeout(timeout)
+
+    class PyLibMCCache(memcached.PyLibMCCache):
+        """
+        PyLibMCCache version that interprets 0 to mean, roughly, 30 days.
+        This is because `pylibmc interprets 0 to mean literally zero seconds
+        <http://sendapatch.se/projects/pylibmc/misc.html#differences-from-python-memcached>`_
+        rather than "infinity" as memcached itself does.  The maximum timeout
+        memcached allows before treating the timeout as a timestamp is just
+        under 30 days.
+        """
+        def _get_memcache_timeout(self, timeout=None):
+            # pylibmc doesn't like our definition of 0
+            if timeout == 0:
+                return 2591999
+            return super(PyLibMCCache, self)._get_memcache_timeout(timeout)
+
+    class PyLibMCCacheSasl(PyLibMCCacheSasl):
         """
         PyLibMCCache version that interprets 0 to mean, roughly, 30 days.
         This is because `pylibmc interprets 0 to mean literally zero seconds
